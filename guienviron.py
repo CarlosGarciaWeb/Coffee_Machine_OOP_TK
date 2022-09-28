a0b19779878facbdb4ab219a13074434c0948bc6
Carlos Garcia <carlos.grh@outlook.com>
2022-09-27 20:40:27 -0500

    add radio buttons of all diferent type of coffees, create method that shows selected type of coffee and bring price from the price selection

--- guienviron.py
+++ guienviron.py
@@ -1,8 +1,14 @@
-from email.mime import image
+from select import select
 import tkinter as tk
 from tkinter import *
+from tokenize import String
 from turtle import width
+from purchase import Prices
 
+
+# purchase section
+
+purchase_method = Prices()
 
 
 #Colors
@@ -14,11 +20,16 @@ Text_color = '#2C3639'
 
 FONT = 'Courier'
 
+
+
+
+
 # UI SET UP
 class GuiEnvironment():
 
     def __init__(self):
         self.window = Tk()
+        self.var = StringVar()
         self.initate_gui()
 
     def initate_gui(self):
@@ -26,6 +37,7 @@ class GuiEnvironment():
         self.window.title('Coffee Machine')
 
         self.window.config(padx=100, pady=50, bg=BG_Softbrown)
+
 
         title_gui = Label(text='Coffee Machine', font=(FONT, 35, 'bold'), bg=BG_Softbrown)
         title_gui.grid(column=0, row=0)
@@ -41,8 +53,27 @@ class GuiEnvironment():
         resource_window_button = Button(text='Resources', highlightthickness=0, command=None, font=(FONT, 24, 'bold'), fg=BG_Button, width=25)
         resource_window_button.grid(column=0, row=4)
 
+        coffee_button = Radiobutton(text='Coffee', highlightthickness=0,  font=(FONT, 24, 'bold'), fg=BG_Button, bg=BG_Softbrown, variable= self.var, value='Coffee')
+        coffee_button.grid(column=1, row=0)
+        flat_white_button = Radiobutton(text='Flat White', highlightthickness=0,  font=(FONT, 25, 'bold'), fg=BG_Button, bg=BG_Softbrown, variable= self.var, value='Flat White')
+        flat_white_button.grid(column=1, row=1)
+        cappucino_button = Radiobutton(text='Cappucino', highlightthickness=0,  font=(FONT, 25, 'bold'), fg=BG_Button, bg=BG_Softbrown, variable= self.var, value='Cappucino')
+        cappucino_button.grid(column=1, row=2)
+        moccacino_button = Radiobutton(text='Moccacino', highlightthickness=0,  font=(FONT, 25, 'bold'), fg=BG_Button, bg=BG_Softbrown, variable= self.var, value='Moccacino')
+        moccacino_button.grid(column=1, row=3)
+        buy_button = Button(text='Buy', highlightthickness=0, command=self.select_item, font=(FONT, 25, 'bold'), fg=BG_Button, bg=BG_Softbrown)
+        buy_button.grid(column=1, row=4)       
+
 
         self.window.mainloop()
+
+    def select_item(self):
+        selection = self.var.get()
+        price = purchase_method.items[selection]
+        print(f'The price for {selection} is {price}')
+
+
+        
 
 
 
---
+++ purchase.py
@@ -0,0 +1,14 @@
+
+
+class Prices():
+
+    def __init__(self):
+        self.coffee = 2.50
+        self.flat_wite = 3.25
+        self.cappucino = 3.00
+        self.moccacino = 3.50
+        self.items = {'Coffee':self.coffee, 'Flat White': self.flat_wite, 'Cappucino': self.cappucino, 'Moccacino': self.moccacino}
+
+
+
+    
